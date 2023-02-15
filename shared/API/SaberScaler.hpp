c2dcf0999e55e8a2c4dd202d87731b5fa688c24f
Millzy <millzyyt08@gmail.com>
2023-02-15 00:14:28 +0000

    API headers (still need to impl)

--- shared/API/SaberScaler.hpp
+++ shared/API/SaberScaler.hpp
@@ -3,6 +3,10 @@
 namespace ThinSaberCore {
     class SaberScaler {
     public:
+        static float getSaberThickness();
+        static void setSaberThickness(float value);
 
+        static float getSaberLength();
+        static void setSaberLength(float value);
     };
 }
